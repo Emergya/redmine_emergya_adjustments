93d355232e54d5823124611f8cab8eedd39df068
jresinas <jresinas@emergya.com>
2015-02-24 12:32:00 +0100

    Show BPO total cost in old BPO tickets

--- assets/javascripts/fields_autofill.js
+++ assets/javascripts/fields_autofill.js
@@ -7,35 +7,40 @@ $(document).ready(function(){
 	
 
 	$('.launcher').live('change', function(){
-		params = {};
-
-		$('.launcher').each(function(index,value){
-			//params += $(this).attr('data-attr_name')+"="+$(this).val()+"&";
-			params[$(this).attr('data-attr_name')] = $(this).val();
-		});
-		//params = encodeURI(params.substring(0, params.length-1));
-		
-		if ($('.autofilled_field').hasClass('select_input')){
-			default_options=new Array();
-			$('.autofilled_field option').each(function(){
-				option = new Array();
-				option.push(this.innerHTML);
-				option.push(this.value);
-				default_options.push(option);
-			});
-			params['options'] = JSON.stringify(default_options);
-		}
-		$.ajax({
-			url: url,
-			data: params,
-			success: function(data){
-				if ($('.autofilled_field').is('input')){
-					$('.autofilled_field').val(data);
-				} else {
-					$('.autofilled_field').html(data);
-				}
-			}
-		});
+		autofill_field();
 	});
 
 });
+
+
+function autofill_field(){
+	params = {};
+
+	$('.launcher').each(function(index,value){
+		//params += $(this).attr('data-attr_name')+"="+$(this).val()+"&";
+		params[$(this).attr('data-attr_name')] = $(this).val();
+	});
+	//params = encodeURI(params.substring(0, params.length-1));
+	
+	if ($('.autofilled_field').hasClass('select_input')){
+		default_options=new Array();
+		$('.autofilled_field option').each(function(){
+			option = new Array();
+			option.push(this.innerHTML);
+			option.push(this.value);
+			default_options.push(option);
+		});
+		params['options'] = JSON.stringify(default_options);
+	}
+	$.ajax({
+		url: url,
+		data: params,
+		success: function(data){
+			if ($('.autofilled_field').is('input')){
+				$('.autofilled_field').val(data);
+			} else {
+				$('.autofilled_field').html(data);
+			}
+		}
+	});
+}
