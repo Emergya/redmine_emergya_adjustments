150033f849a3d03addc495d28c00fe4cd262e525
jresinas <jresinas@emergya.com>
2017-12-20 11:00:12 +0100

    Allow to autocalculate multiple fields in the same tracker

--- assets/javascripts/fields_autofill.js
+++ assets/javascripts/fields_autofill.js
@@ -1,45 +1,51 @@
 $(document).ready(function(){
-/*
-	$('.autofilled_field').live('focus', function(){
-		$(this).blur();
-	});
-*/
-	
 	$(document).on('change', '.launcher', function(){
-		autofill_field();
+		autofill_field($(this).data('attr_slave'));
 	});
 
 });
 
-function autofill_field(){
-	params = {};
+function autofill_field(args){
+	args = args.split(',');
 
-	$('.launcher').each(function(index,value){
-		//params += $(this).attr('data-attr_name')+"="+$(this).val()+"&";
-		params[$(this).attr('data-attr_name')] = $(this).val();
-	});
-	//params = encodeURI(params.substring(0, params.length-1));
-	
-	if ($('.autofilled_field').hasClass('select_input')){
-		default_options=new Array();
-		$('.autofilled_field option').each(function(){
-			option = new Array();
-			option.push(this.innerHTML);
-			option.push(this.value);
-			default_options.push(option);
+	for(index in args){
+		slave = args[index];
+		params = {};
+
+		$('.launcher').each(function(index,value){
+			slaves = $(this).data('attr_slave').split(',');
+			if (slaves.includes(slave)){
+				params[$(this).data('attr_name')] = $(this).val();
+			}
 		});
-		params['options'] = JSON.stringify(default_options);
+
+		if ($('.autofilled_field[data-attr_name="'+slave+'"]').hasClass('select_input')){
+			default_options=new Array();
+			$('.autofilled_field[data-attr_name="'+slave+'"] option').each(function(){
+				option = new Array();
+				option.push(this.innerHTML);
+				option.push(this.value);
+				default_options.push(option);
+			});
+			params['options'] = JSON.stringify(default_options);
+		}
+
+
+		set_value(slave);
 	}
+}
+
+function set_value(slave){
 	$.ajax({
 		url: url,
 		data: params,
 		success: function(data){
-			if ($('.autofilled_field').is('input')){
-				$('.autofilled_field').val(data);
+			if ($('.autofilled_field[data-attr_name="'+slave+'"]').is('input')){
+				$('.autofilled_field[data-attr_name="'+slave+'"]').val(data);
 			} else {
-				$('.autofilled_field').html(data);
+				$('.autofilled_field[data-attr_name="'+slave+'"]').html(data);
 			}
-			$('.autofilled_field').trigger('change');
+			$('.autofilled_field[data-attr_name="'+slave+'"]').trigger('change');
 		}
 	});
 }
